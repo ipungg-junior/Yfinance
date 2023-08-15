dd01d3b231580f792a0460d589bb61e42e0047b2
ipungg-junior <ipungg.id@gmail.com>
2023-08-15 10:47:10 +0700

    Clean and Build base apps

--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_control
 
-# Create your views here.
+@method_decorator(cache_control(no_cache=True, must_revalidate=True), name='dispatch')
+class Landing(View):
+
+    def main(request):
+        return HttpResponse(status=200)
